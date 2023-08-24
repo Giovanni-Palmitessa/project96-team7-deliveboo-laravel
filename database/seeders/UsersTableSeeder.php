f0e2f96b4afb404fd6b9cddcb5d4c52fa947dad2
DavideFarci <yank92.df@gmail.com>
2023-08-24 16:44:11 +0200

    fix array users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,24 +20,28 @@ class UsersTableSeeder extends Seeder
                 "email"     => "davide.farci@gmail.com",
                 "password"  => Hash::make('davide96'),
                 "image"     => null,
+                "restaurant_id" => "1",
             ],
             [
                 "name"      => "Domenico Ferrari",
                 "email"     => "domenico.ferrari@gmail.com",
                 "password"  => Hash::make('domenico96'),
                 "image"     => null,
+                "restaurant_id" => "2",
             ],
             [
                 "name"      => "Loris Marzocchi",
                 "email"     => "loris.marzocchi@gmail.com",
                 "password"  => Hash::make('loris96'),
                 "image"     => null,
+                "restaurant_id" => "3",
             ],
             [
                 "name"      => "Giovanni Palmitessa",
                 "email"     => "giovanni.palmitessa@gmail.com",
                 "password"  => Hash::make('giovanni96'),
                 "image"     => null,
+                "restaurant_id" => "4",
             ],
         ];
        
