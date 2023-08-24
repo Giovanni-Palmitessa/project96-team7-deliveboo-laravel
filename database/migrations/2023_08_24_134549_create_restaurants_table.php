103ac9b22526c85619437905b5c6d376b80be148
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-24 16:35:28 +0200

    complete migration for restaurants

--- database/migrations/2023_08_24_134549_create_restaurants_table.php
+++ database/migrations/2023_08_24_134549_create_restaurants_table.php
@@ -15,7 +15,25 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->string('city', 30);
+            $table->string('address', 50);
+            $table->string('vat', 10);
+            $table->string('url_image', 300)->nullable();
+            $table->smallInteger('priceRange')->nullable();
+            $table->tinyInteger('rating_value')->nullable();
+            $table->smallInteger('review_count')->nullable();
+
+            // creare la colonna della chiave esterna
+            $table->unsignedBigInteger('product_ID')->after('id');
+            $table->unsignedBigInteger('user_ID')->after('product_ID');
+            $table->unsignedBigInteger('order_ID')->after('user_ID');
+
+            // definire la colonna come chiave esterna
+            $table->foreign('product_ID')->references('id')->on('products');
+            $table->foreign('user_ID')->references('id')->on('users');
+            $table->foreign('order_ID')->references('id')->on('orders');
         });
     }
 
@@ -26,6 +44,16 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('restaurants');
+        Schema::dropIfExists('restaurants' , function (Blueprint $table) {
+            // eliminare la chiave esterna
+            $table->dropForeign('restaurants_product_id_foreign');
+            $table->dropForeign('restaurants_user_id_foreign');
+            $table->dropForeign('restaurants_order_id_foreign');
+
+            // eliminare la colonna
+            $table->dropColumn('product_ID');
+            $table->dropColumn('user_ID');
+            $table->dropColumn('order_ID');
+        });
     }
 };
