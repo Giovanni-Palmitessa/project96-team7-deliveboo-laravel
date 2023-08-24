8397b4648d67e5f66360054f56e0cf36b8257a35
LorisMarzocchi <ml77182a@gmail.com>
2023-08-24 16:56:48 +0200

    add category

--- database/migrations/2023_08_24_135312_create_categories_table.php
+++ database/migrations/2023_08_24_135312_create_categories_table.php
@@ -13,24 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-
+     *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('categories');
     }
 };
