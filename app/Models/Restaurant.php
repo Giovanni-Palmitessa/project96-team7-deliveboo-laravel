e01c7b9763f59a3ee9785ca842a155a158017abd
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-24 17:36:04 +0200

    adjust restaurant relations

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -2,12 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Restaurant extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    public function user() {
+        return $this->hasOne(User::class);
+    }
+
+    public function products() {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function orders() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
 }
