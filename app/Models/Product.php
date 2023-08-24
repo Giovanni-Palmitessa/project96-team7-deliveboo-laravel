5c5a70c36c4f9e71e4b5fe498e3095b9158386c3
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-24 17:26:43 +0200

    adjust relations for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class);
     }
+
+    public function restaurant() 
+    {
+        return $this->hasMany(Restaurant::class);
+    }
 }
