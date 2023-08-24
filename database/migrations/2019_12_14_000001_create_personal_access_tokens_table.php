b70798fe9534ea62ef65fa2f4518c5f253039bf8
DavideFarci <yank92.df@gmail.com>
2023-08-24 16:14:41 +0200

    mod users migration

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('image')->nullable();
+            $table->morphs('tokenable');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
