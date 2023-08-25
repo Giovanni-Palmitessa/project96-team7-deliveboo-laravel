145c2c00faf521cd73bc4f1afff427f3de36ee91
DavideFarci <yank92.df@gmail.com>
2023-08-25 15:34:18 +0200

    set api routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::get('products', [ProductController::class, 'index'])->name('api.products.index');
+Route::get('products/{product}', [ProductController::class, 'show'])->name('api.products.show');
+
+Route::get('restaurants', [RestaurantController::class, 'index'])->name('api.restaurants.index');
+Route::get('restaurants/{restaurant}', [RestaurantController::class, 'show'])->name('api.restaurants.show');
+
+Route::get('categories', [CategoryController::class, 'index'])->name('api.categories.index');
