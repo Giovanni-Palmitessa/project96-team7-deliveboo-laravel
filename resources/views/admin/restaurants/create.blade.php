f16b238b26d1b7013a97d15ae12c89f64fc72152
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-25 16:49:03 +0200

    add csrf

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -2,6 +2,8 @@
 
 
 <form method="POST" action="{{ route('admin.restaurants.store') }}">
+    @csrf
+    
     <div class="mb-6">
       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
       <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
