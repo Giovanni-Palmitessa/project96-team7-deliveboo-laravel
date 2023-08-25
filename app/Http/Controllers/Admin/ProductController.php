d9349ee1d7d4f8f9638de0eefb9075ea2943504f
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-08-25 16:16:50 +0200

    add create

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -10,6 +10,28 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    private $validations = [
+        'name' => 'required|string|min:2|max:50',
+        'ingredients' => 'required|string',
+        'price' => 'required|string|integer',
+        'description' => 'required|string',
+        'url_image' => 'required|string',
+    ];
+
+    private $validation_messages = [
+        // name
+        'name.required' => 'Il campo Nome è obbligatorio',
+        'name.min' => 'Il campo Nome deve avere almeno :min caratteri',
+        'name.max' => 'Il campo Nome deve avere massimo :max caratteri',
+        // description
+        'description.required' => 'Il campo Descrizione è obbligatorio',
+        // url_image
+        'url_image.required' => 'Il campo Url Immagine è obbligatorio',
+        // price
+        'price.integer' => 'Il prezzo deve essere un\' intero',
+    ];
+
     public function index()
     {
         $products = Product::paginate(5);
@@ -18,17 +40,34 @@ class ProductController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.products.create');
     }
 
     public function store(Request $request)
     {
-        //
+        $request->validate($this->validations, $this->validation_messages);
+
+        $data = $request->all();
+
+        $newProduct = new Product();
+        $newProduct->name = $data['name'];
+        $newProduct->slug = Product::slugger($data['name']);
+        $newProduct->ingredients = $data['ingredients'];
+        $newProduct->price = $data['price'];
+        $newProduct->description = $data['description'];
+        $newProduct->url_image = $data['url_image'];
+
+        // salvo il nuovo Prodotto
+        $newProduct->save();
+
+        // redirect
+        return to_route('admin.product.show', ['product' => $newProduct]);
     }
 
-    public function show(Product $product)
+    public function show($slug)
     {
-        //
+        $product = Product::where('slug', $slug)->firstOrFail();
+        return view('admin.product.show', compact('product'));
     }
 
     public function edit(Product $product)
