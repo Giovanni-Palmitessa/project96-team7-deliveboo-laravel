7d497044a7e43c8f63571d926194936521b10a85
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-25 16:27:05 +0200

    complete method for edit

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -8,6 +8,29 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
+    private $validations = [
+        'name' => 'required|string|max:50',
+        'description' => 'required|string',
+        'city' => 'required|string|max:30',
+        'address' => 'required|string|max:50',
+        'vat' => 'required|string|max:10|min:10',
+        'url_image' => 'nullable|url|max:300',
+        'priceRange' => 'nullable|integer',
+        'rating_value' => 'nullable|integer',
+        'review_count' => 'nullable|integer',
+        'categories' => 'nullable|array',
+        'categories.*' => 'integer|exists:categories,id',
+    ];
+
+    private $validations_messages = [
+        'required' => 'Il campo :attribute è richiesto',
+        'min' => 'Il campo :attribute deve avere almeno :min caratteri',
+        'max' => 'Il campo :attribute deve avere massimo :max caratteri',
+        'url' => 'Il campo :attribute deve essere un URL valido',
+        'date' => 'Il campo :attribute deve essere una data in formato valido',
+        'exists' => 'Il campo :attribute non è valido',
+        'integer' => 'Il campo :attribute deve essere un numero intero.',
+    ];
     /**
      * Display a listing of the resource.
      *
@@ -61,21 +84,23 @@ class RestaurantController extends Controller
         $newRestaurant = new Restaurant();
         $newRestaurant->name = $data['name'];
         $newRestaurant->description = $data['description'];
-        $newRestaurant->type_id = $data['type_id'];
+        $newRestaurant->city = $data['city'];
+        $newRestaurant->address = $data['address'];
+        // $newRestaurant->image = $imagePath;
+        $newRestaurant->vat = $data['vat'];
         $newRestaurant->url_image = $data['url_image'];
-        $newRestaurant->image = $imagePath;
-        $newRestaurant->pickup_date = $data['pickup_date'];
-        $newRestaurant->deploy_date = $data['deploy_date'];
-        $newRestaurant->description = $data['description'];
+        $newRestaurant->priceRange = $data['description'];
+        $newRestaurant->rating_value = $data['rating_value'];
+        $newRestaurant->review_count = $data['review_count'];
 
         $newRestaurant->save();
 
         // associare i tag
-        $newPortfolio->technologies()->sync($data['technologies'] ?? []);
+        $newRestaurant->categories()->sync($data['categories'] ?? []);
 
         // reindirizzare su una rotta di tipo get
 
-        return to_route('admin.portfolios.show', ['portfolio' => $newPortfolio]);
+        return to_route('admin.restaurants.show', ['restaurant' => $newRestaurant]);
     }
 
     /**
@@ -84,9 +109,11 @@ class RestaurantController extends Controller
      * @param  \App\Models\Restaurant  $restaurant
      * @return \Illuminate\Http\Response
      */
-    public function show(Restaurant $restaurant)
+    public function show($slug)
     {
-        //
+        $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
+        
+        return view('admin.restaurants.show', compact('restaurant'));
     }
 
     /**
@@ -95,9 +122,14 @@ class RestaurantController extends Controller
      * @param  \App\Models\Restaurant  $restaurant
      * @return \Illuminate\Http\Response
      */
-    public function edit(Restaurant $restaurant)
+    public function edit($slug)
     {
-        //
+        $restaurant = Restaurant::where('slug', $slug)->firstOrFail();
+
+        $products = Product::all();
+        $categories = Category::all();
+        
+        return view('admin.restaurants.edit', compact('restaurant', 'products', 'categories'));
     }
 
     /**
---
+++ resources/views/admin/restaurants/show.blade.php
@@ -0,0 +1,1 @@
+<h1>Sono lo Show</h1>
