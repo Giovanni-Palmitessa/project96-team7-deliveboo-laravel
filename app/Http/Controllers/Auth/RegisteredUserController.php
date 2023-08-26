54da1e27838faf64394e7fcc33faf0ca859dc4a5
DavideFarci <yank92.df@gmail.com>
2023-08-26 23:02:53 +0200

    reset last commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
-use App\Models\Category;
-use Illuminate\View\View;
-use App\Models\Restaurant;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rules;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Auth\Events\Registered;
-use App\Providers\RouteServiceProvider;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
 
 class RegisteredUserController extends Controller
 {
@@ -22,7 +20,6 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        $category = Category::all();
         return view('auth.register');
     }
 
@@ -34,32 +31,15 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name'            => ['required', 'string', 'max:255'],
-            'email'           => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password'        => ['required', 'confirmed', Rules\Password::defaults()],
-
-            'restaurant_name' => ['required', 'string', 'max:255'],
-            'description'     => ['required', 'string', 'max:1000'],
-            'city'            => ['required', 'string', 'max:50'],
-            'address'         => ['required', 'string', 'max:150'],
-            'vat'             => ['required', 'string', 'min:10', 'max:10'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
-        $restaurant = Restaurant::create([
-            'restaurant_name'   =>  $request->restaurant_name,
-            'slug'              =>  Restaurant::slugger($request['restaurant_name']),   
-            'description'       =>  $request->description,  
-            'city'              =>  $request->city,
-            'address'           =>  $request->address,
-            'vat'               =>  $request->vat,
-        ]);
-        $restaurant->categories()->sync($request['categories'] ?? []);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'restaurant_id' =>$restaurant->id,
         ]);
 
         event(new Registered($user));
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,41 +39,6 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <!-- Restaurant Name -->
-        <div>
-            <x-input-label for="restaurant_name" :value="'restaurant_name'" />
-            <x-text-input id="restaurant_name" class="block mt-1 w-full" type="text" restaurant_name="restaurant_name" :value="old('restaurant_name')" required autofocus autocomplete="restaurant_name" />
-            <x-input-error :messages="$errors->get('restaurant_name')" class="mt-2" />
-        </div>
-
-        <!-- Description -->
-        <div>
-            <x-input-label for="description" :value="'description'" />
-            <x-text-input id="description" class="block mt-1 w-full" type="text" description="description" :value="old('description')" required autofocus autocomplete="description" />
-            <x-input-error :messages="$errors->get('description')" class="mt-2" />
-        </div>
-
-        <!-- City -->
-        <div>
-            <x-input-label for="city" :value="'city'" />
-            <x-text-input id="city" class="block mt-1 w-full" type="text" city="city" :value="old('city')" required autofocus autocomplete="city" />
-            <x-input-error :messages="$errors->get('city')" class="mt-2" />
-        </div>
-
-        <!-- Address -->
-        <div>
-            <x-input-label for="address" :value="'address'" />
-            <x-text-input id="address" class="block mt-1 w-full" type="text" address="address" :value="old('address')" required autofocus autocomplete="address" />
-            <x-input-error :messages="$errors->get('address')" class="mt-2" />
-        </div>
-
-        <!-- Vat -->
-        <div>
-            <x-input-label for="vat" :value="'vat'" />
-            <x-text-input id="vat" class="block mt-1 w-full" type="text" vat="vat" :value="old('vat')" required autofocus autocomplete="vat" />
-            <x-input-error :messages="$errors->get('vat')" class="mt-2" />
-        </div>
-
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
