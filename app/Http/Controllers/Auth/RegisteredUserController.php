81a18e4901907f7a60c9f64268b0822ed49d2d75
DavideFarci <yank92.df@gmail.com>
2023-08-26 22:10:19 +0200

    fix funct. store to create new restaurant. Call categories

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
+use App\Models\Category;
+use Illuminate\View\View;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
 
 class RegisteredUserController extends Controller
 {
@@ -20,6 +22,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
+        $category = Category::all();
         return view('auth.register');
     }
 
@@ -31,15 +34,41 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name'            => ['required', 'string', 'max:255'],
+            'email'           => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password'        => ['required', 'confirmed', Rules\Password::defaults()],
+
+            'restaurant_name' => ['required', 'string', 'max:255'],
+            'description'     => ['required', 'string', 'max:1000'],
+            'city'            => ['required', 'string', 'max:50'],
+            'address'         => ['required', 'string', 'max:150'],
+            'vat'             => ['required', 'numeric', 'max:10'],
+            'url_image'       => ['required', 'string', 'max:500'],
+            'priceRange'      => ['required', 'numeric', 'max:200'],
+            'rating_value'    => ['required', 'numeric', 'max:250'],
+            'review_count'    => ['required', 'numeric', 'max:250'],
         ]);
+
+        $restaurant = Restaurant::create([
+            'restaurant_name'   =>  $request->restaurant_name,
+            'slug'              =>  Restaurant::slugger($request['restaurant_name']),   
+            'description'       =>  $request->description,  
+            'city'              =>  $request->city,
+            'address'           =>  $request->address,
+            'vat'               =>  $request->vat,
+            'url_image'         =>  $request->url_image,
+            'priceRange'        =>  $request->priceRange,
+            'rating_value'      =>  $request->rating_value,
+            'review_count'      =>  $request->review_count,
+
+        ]);
+        $restaurant->categories()->sync($request['categories'] ?? []);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'restaurant_id' =>$restaurant->id,
         ]);
 
         event(new Registered($user));
