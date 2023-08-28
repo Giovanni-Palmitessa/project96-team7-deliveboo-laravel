bfe576ee2301957871915c4fb1601b3c69ea67ac
LorisMarzocchi <ml77182a@gmail.com>
2023-08-28 13:11:56 +0200

    add login validation

--- resources/js/loginValidation.js
+++ resources/js/loginValidation.js
@@ -1,42 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Identificare il form di registrazione
-    const registerForm = document.querySelector("#registerForm");
+    // Identificare il form di login
+    const loginForm = document.querySelector("#loginForm");
 
-    if (!registerForm) return;
+    if (!loginForm) return;
 
-    // Ottieni gli elementi input all'interno del form di registrazione
-    const nome = registerForm.querySelector("#reg_name");
-    const email = registerForm.querySelector("#reg_email");
-    const password = registerForm.querySelector("#reg_password");
-    const passwordConferma = registerForm.querySelector(
-        "#reg_password_confirmation"
-    );
+    // Ottieni gli elementi input all'interno del form di login
+    const email = loginForm.querySelector("#log_email");
+    const password = loginForm.querySelector("#log_password");
 
     // Assicurarsi che gli input esistano prima di proseguire
-    if (!nome || !email || !password || !passwordConferma) return;
+    if (!email || !password) return;
 
-    const nomeErrori = {
-        lunghezza: "Il nome deve contenere almeno 3 caratteri",
-    };
-
-    const emailErrori = {
+    const emailError = {
         "@": "La email deve contenere una '@'",
         ".suffisso": "La email deve contenere un prefisso '.com' o '.it'",
         lunghezza: "Inserisci un'email valida",
     };
 
-    const passwordErrori = {
+    const passwordError = {
         lunghezza: "La password deve contenere almeno 8 caratteri",
     };
 
-    const passwordConfermaErrori = {
-        nonCorrispondente: "Le password non corrispondono",
-    };
-
-    nome.addEventListener("input", validaNome);
     email.addEventListener("input", validaEmail);
     password.addEventListener("input", validaPassword);
-    passwordConferma.addEventListener("input", validaConfermaPassword);
 
     function creaElementoErrore() {
         const span = document.createElement("span");
@@ -45,21 +31,13 @@ document.addEventListener("DOMContentLoaded", function () {
         return span;
     }
 
-    function validaNome() {
-        if (nome.value.length < 3) {
-            mostraErrore(nome, nomeErrori["lunghezza"]);
-        } else {
-            rimuoviErrore(nome);
-        }
-    }
-
     function validaEmail() {
         if (!email.value.includes("@")) {
-            mostraErrore(email, emailErrori["@"]);
+            mostraErrore(email, emailError["@"]);
         } else if (!suffissoValido(email.value)) {
-            mostraErrore(email, emailErrori[".suffisso"]);
+            mostraErrore(email, emailError[".suffisso"]);
         } else if (email.value.length <= 5) {
-            mostraErrore(email, emailErrori["lunghezza"]);
+            mostraErrore(email, emailError["lunghezza"]);
         } else {
             rimuoviErrore(email);
         }
@@ -71,20 +49,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function validaPassword() {
         if (password.value.length <= 7) {
-            mostraErrore(password, passwordErrori["lunghezza"]);
+            mostraErrore(password, passwordError["lunghezza"]);
         } else {
             rimuoviErrore(password);
-        }
-    }
-
-    function validaConfermaPassword() {
-        if (passwordConferma.value !== password.value) {
-            mostraErrore(
-                passwordConferma,
-                passwordConfermaErrori["nonCorrispondente"]
-            );
-        } else {
-            rimuoviErrore(passwordConferma);
         }
     }
 
