339467065ff3f8ab8af767d15c97f3cb294e7dd1
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-28 18:10:53 +0200

    adjust dashboard page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,25 +1,6 @@
 @extends('admin.layouts.base')
-@section('contents')
-    <h1 class="text-4xl text-b_hover">DASHBOARD</h1>
-    
-    <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+@section('contents')    
     <div class="min-h-screen bg-gray-100 p-6 flex flex-col items-center">
-
         <!-- Header del ristorante -->
         <div class="text-center mb-10">
             <h1 class="text-4xl font-bold text-gray-800 mb-4">{{ $restaurant->name }}</h1>
@@ -54,8 +35,7 @@
                 @endforeach
             </ul>
         </div>
-
     </div>
-</x-app-layout>
+
 @endsection
 
