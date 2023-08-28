7c80061dc409afce7ef334717d7f78aacdacfc99
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-08-28 17:21:49 +0200

    fix validation's edit

--- resources/js/productValidationCreate.js
+++ resources/js/productValidationCreate.js
@@ -30,45 +30,48 @@ if (productCreateForm) {
         let isValid = true;
 
         if (name.trim() === "") {
-            nameError.textContent = "Il campo Nome è obbligatorio ciao ciao.";
+            nameError.textContent = "Il campo Nome è obbligatorio.";
             isValid = false;
         } else if (name.length < 2) {
-            nameError.textContent = "Il campo Nome è troppo corto ciao ciao.";
+            nameError.textContent = "Il campo 'Nome' deve contenere almeno 2 caratteri.";
             isValid = false;
         } else if (name.length > 50) {
-            nameError.textContent = "Il campo Nome è troppo lungo ciao ciao.";
+            nameError.textContent = "Il campo 'Nome' può contenere al massimo 50 caratteri.";
             isValid = false;
         }
 
         if (ingredients.trim() === "") {
-            ingredientsError.textContent = "Il campo Ingredienti è obbligatorio ciao a tutti";
+            ingredientsError.textContent = "Il campo 'Ingredienti' è obbligatorio.";
             isValid = false;
         } else if (ingredients.length < 10) {
-            ingredientsError.textContent = "Il campo Ingredienti deve contenere almeno 10 caratteri";
+            ingredientsError.textContent = "Il campo 'Ingredienti' deve contenere almeno 10 caratteri.";
             isValid = false;
         }
 
         if (price.trim() === "") {
-            priceError.textContent = "Il campo prezzo è obbligatorio bella";
+            priceError.textContent = "Il campo 'Prezzo' è obbligatorio.";
             isValid = false;
         } else if (price <= 0) {
-            priceError.textContent = "Il prezzo deve essere maggiore di zero";
+            priceError.textContent = "Il campo 'Prezzo' non può essere zero.";
+            isValid = false;
+        } else if (price > 630) {
+            priceError.textContent = "Il campo 'Prezzo' può essere al massimo di 630."
             isValid = false;
         }
 
         if (description.trim() === "") {
-            descriptionError.textContent = "Il campo descrizione è obbligatoriooo";
+            descriptionError.textContent = "Il campo 'Descrizione' è obbligatorio.";
             isValid = false;
         } else if (description.length < 10) {
-            descriptionError.textContent = "Il campo descrizione deve contenere almeno 10 caratteri"
+            descriptionError.textContent = "Il campo 'Descrizione' deve contenere almeno 10 caratteri."
             isValid = false;
         }
 
         if (url_image.trim() === "") {
-            urlImageError.textContent = "Il campo Url_image è obbligatoriooo";
+            urlImageError.textContent = "Il campo 'Immagine' è obbligatorio.";
             isValid = false;
         } else if (url_image.length < 10) {
-            urlImageError.textContent = "Il campo Url_image deve contenere almeno 10 caratteri";
+            urlImageError.textContent = "Il campo 'Immagine' deve contenere almeno 10 caratteri.";
             isValid = false;
         }
 
