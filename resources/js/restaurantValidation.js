0dad0f37c8bbadbc0172fd7b5e6ff37f9e8fdbd7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-28 15:35:52 +0200

    test for validation

--- resources/js/restaurantValidation.js
+++ resources/js/restaurantValidation.js
@@ -1,44 +1,46 @@
 // validation restaurants
 
-const form = document.getElementById("form-create"); // Seleziona il form
+const formCreate = document.getElementById("form-create"); // Seleziona il form
 
-form.addEventListener("submit", function (event) {
-    // Impedisci il comportamento di default (ricaricare la pagina)
-    event.preventDefault();
+if (formCreate) {
+    formCreate.addEventListener("submit", function (event) {
+        // Impedisci il comportamento di default (ricaricare la pagina)
+        event.preventDefault();
 
-    // Ottieni i valori dai campi di input
-    const name = document.getElementById("name-create").value;
-    const description = document.getElementById("description-create").value;
-    // Aggiungi qui gli altri campi...
+        // Ottieni i valori dai campi di input
+        const name = document.getElementById("name-create").value;
+        const description = document.getElementById("description-create").value;
+        // Aggiungi qui gli altri campi...
 
-    // Ottieni le aree in cui verranno mostrati i messaggi di errore
-    const nameError = document.getElementById("nameError");
-    const descriptionError = document.getElementById("descriptionError");
-    // Aggiungi qui gli altri elementi per gli errori...
+        // Ottieni le aree in cui verranno mostrati i messaggi di errore
+        const nameError = document.getElementById("nameError");
+        const descriptionError = document.getElementById("descriptionError");
+        // Aggiungi qui gli altri elementi per gli errori...
 
-    // Resetta i messaggi di errore
-    nameError.textContent = "";
-    descriptionError.textContent = "";
-    // Aggiungi qui il reset per gli altri messaggi...
+        // Resetta i messaggi di errore
+        nameError.textContent = "";
+        descriptionError.textContent = "";
+        // Aggiungi qui il reset per gli altri messaggi...
 
-    // Esegui le validazioni
-    let isValid = true;
+        // Esegui le validazioni
+        let isValid = true;
 
-    if (name.trim() === "") {
-        nameError.textContent = "Il campo Nome è obbligatorio ciao ciao.";
-        isValid = false;
-    }
+        if (name.trim() === "") {
+            nameError.textContent = "Il campo Nome è obbligatorio ciao ciao.";
+            isValid = false;
+        }
 
-    if (description.trim() === "") {
-        descriptionError.textContent =
-            "Il campo Descrizione è obbligatorio ciao ciao 2.";
-        isValid = false;
-    }
+        if (description.trim() === "") {
+            descriptionError.textContent =
+                "Il campo Descrizione è obbligatorio ciao ciao 2.";
+            isValid = false;
+        }
 
-    // Aggiungi qui le altre validazioni...
+        // Aggiungi qui le altre validazioni...
 
-    // Se tutto è valido, sottometti il form
-    if (isValid) {
-        form.submit();
-    }
-});
+        // Se tutto è valido, sottometti il form
+        if (isValid) {
+            formCreate.submit();
+        }
+    });
+}
--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -132,4 +132,9 @@ novalidate
 
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
   </form>
+
+  {{-- @push('scripts')
+        <script src="{{ asset('js/restaurantValidation.js') }}"></script>
+  @endpush --}}
+
 @endsection
