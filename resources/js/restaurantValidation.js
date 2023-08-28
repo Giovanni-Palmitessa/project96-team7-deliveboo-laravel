d3e8bc42ee8314d575e9c5785f18667ecd619a12
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-28 15:40:29 +0200

    add js address validation

--- resources/js/restaurantValidation.js
+++ resources/js/restaurantValidation.js
@@ -11,16 +11,19 @@ if (formCreate) {
         const name = document.getElementById("name-create").value;
         const description = document.getElementById("description-create").value;
         const city = document.getElementById("city-create").value;
+        const address = document.getElementById("address-create").value;
 
         // Ottieni le aree in cui verranno mostrati i messaggi di errore
         const nameError = document.getElementById("nameError");
         const descriptionError = document.getElementById("descriptionError");
         const cityError = document.getElementById("cityError");
+        const addressError = document.getElementById("addressError");
 
         // Resetta i messaggi di errore
         nameError.textContent = "";
         descriptionError.textContent = "";
         cityError.textContent = "";
+        addressError.textContent = "";
 
         // Esegui le validazioni
         let isValid = true;
@@ -39,6 +42,12 @@ if (formCreate) {
         if (city.trim() === "") {
             cityError.textContent =
                 "Il campo Città è obbligatorio ciao ciao 2.";
+            isValid = false;
+        }
+
+        if (city.trim() === "") {
+            addressError.textContent =
+                "Il campo Indirizzo è obbligatorio ciao ciao 2.";
             isValid = false;
         }
 
