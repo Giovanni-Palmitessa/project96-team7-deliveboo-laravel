28a6ec441669f32cf3e288df3126267eccccfd96
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-28 17:15:09 +0200

    add all error messages in edit

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -30,39 +30,63 @@ id="form-edit"
       <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrizione</label>
       <input type="text" 
       name="description" 
-      id="description" 
+      id="description-edit" 
       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
       value="{{ old('description', $restaurant->description) }}"
+      @error('description') is-invalid @enderror
       >
+      @error('description')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+      @enderror
+      <div class="mb-2 text-sm text-red-600" id="descriptionErrorEdit"></div>
     </div>
 
     <div class="mb-6">
         <label for="city"  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Città</label>
         <input type="text" 
         name="city" 
-        id="city" 
+        id="city-edit" 
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
         value="{{ old('city', $restaurant->city) }}"
+        @error('city-edit') is-invalid @enderror
         >
+        <div class="mb-2 text-sm text-red-600" id="cityErrorEdit"></div>
       </div>
 
       <div class="mb-6">
         <label for="address"  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Indirizzo</label>
         <input type="text" 
         name="address" 
-        id="address" 
+        id="address-edit" 
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
         value="{{ old('address', $restaurant->address) }}"
+        @error('address') is-invalid @enderror
         >
+        @error('address')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+        @enderror
+        <div class="mb-2 text-sm text-red-600" id="addressErrorEdit"></div>
       </div>
 
       <div class="mb-6">
         <label for="vat"  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">P. IVA</label>
         <input type="text" 
-        name="vat" id="vat" 
+        name="vat" 
+        id="vat-edit" 
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
         value="{{ old('vat', $restaurant->vat) }}"
+        @error('vat') is-invalid @enderror
         >
+        @error('vat')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+        @enderror
+        <div class="mb-2 text-sm text-red-600" id="vatErrorEdit"></div>
       </div>
 
       <div class="mb-6">
@@ -105,6 +129,8 @@ id="form-edit"
                 @endforeach
 
                 </fieldset>
+
+                <div class="mb-2 text-sm text-red-600" id="categoryErrorEdit"></div>
       </div>
 
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
