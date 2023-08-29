ddb4d9f45bf92c5afe03656b7ff0e31dd00f1c24
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-29 09:55:52 +0200

    change file config for visible in products

--- config/products.php
+++ config/products.php
@@ -8,6 +8,7 @@ return [
         "description"   => "Un piatto ricco e avvolgente, il Pollo al Curry Cremoso unisce pezzi di pollo teneri e succulenti a una salsa di curry vellutata. Servito su un letto di riso aromatico, è un comfort food con un tocco esotico",
         "url_image"         => "uploads/ricetta1.jpg",
         "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Insalata di Quinoa Mediterranea",
@@ -15,7 +16,8 @@ return [
         "price"         => 10,
         "description"   => "Un'insalata fresca e nutriente che fonde la quinoa con sapori mediterranei. Pomodori dolci, cetrioli croccanti, olive salate e feta cremosa si uniscono per creare un'esplosione di gusto",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Tagliatelle al Salmone Affumicato",
@@ -23,7 +25,8 @@ return [
         "price"         => 16,
         "description"   => "Un piatto elegante e delizioso, le Tagliatelle al Salmone Affumicato sono una fusione di pasta fresca e salmone delicato. Una salsa leggera al limone completa questa prelibatezza",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Risotto ai Funghi Porcini",
@@ -31,7 +34,8 @@ return [
         "price"         => 18,
         "description"   => "Un comfort food di lusso, il Risotto ai Funghi Porcini unisce il ricco sapore dei funghi porcini con la cremosità del risotto. Un piatto caldo e avvolgente per le serate fresche",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Tacos di Pollo alla Griglia",
@@ -39,7 +43,8 @@ return [
         "price"         => 8,
         "description"   => "I Tacos di Pollo alla Griglia portano il sapore dell'estate nel tuo piatto. Il pollo marinato e grigliato è avvolto in tortillas calde e accompagnato da una selezione di salse e condimenti",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Insalata Caprese",
@@ -47,7 +52,8 @@ return [
         "price"         => 8,
         "description"   => "L'Insalata Caprese è un classico italiano che celebra i sapori semplici e freschi. Mozzarella di bufala, pomodori e basilico si fondono con olio d'oliva extravergine per una combinazione irresistibile",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Zuppa di Lenticchie Rosse",
@@ -55,7 +61,8 @@ return [
         "price"         => 10,
         "description"   => "Nutriente e confortante, la Zuppa di Lenticchie Rosse è ricca di proteine e fibre. Le lenticchie si uniscono a verdure e spezie per creare una zuppa dal sapore avvolgente",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Omelette Primaverile",
@@ -63,7 +70,8 @@ return [
         "price"         => 7,
         "description"   => "L'Omelette Primaverile è una colazione sana e colorata. Uova leggere si combinano con asparagi croccanti, piselli dolci e formaggio feta per iniziare la giornata con energia",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 3
+        "restaurant_id" => 3,
+        "visible"       => true,
     ],
     [
         "name"          => "Spaghetti alla Carbonara",
@@ -71,7 +79,8 @@ return [
         "price"         => 12,
         "description"   => "Un classico italiano, gli Spaghetti alla Carbonara combinano uova, pancetta croccante e formaggio pecorino. Una pasta cremosa e saporita che conquista ogni palato",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Gamberi Aglio e Limone",
@@ -79,7 +88,8 @@ return [
         "price"         => 18,
         "description"   => "I Gamberi Aglio e Limone sono una deliziosa prelibatezza marina. Gamberi succosi vengono cotti con aglio fragrante e succo di limone per un piatto fresco e leggero",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Insalata di Riso Mediterranea",
@@ -87,7 +97,8 @@ return [
         "price"         => 10,
         "description"   => "Un'insalata completa e saporita, l'Insalata di Riso Mediterranea unisce riso integrale con pomodori, cetrioli, olive e feta, il tutto condito con un dressing all'olio d'oliva",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Pollo al Rosmarino con Patate",
@@ -95,7 +106,8 @@ return [
         "price"         => 15,
         "description"   => "Un piatto rustico e aromatico, il Pollo al Rosmarino con Patate unisce pezzi di pollo succulenti con patate arrosto profumate al rosmarino. Un comfort food dal sapore intenso",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Salmone alla Griglia con Salsa di Mango",
@@ -103,7 +115,8 @@ return [
         "price"         => 16,
         "description"   => "Il Salmone alla Griglia con Salsa di Mango è una combinazione di sapori freschi e contrastanti. Il salmone grigliato è arricchito da una salsa di mango dolce e piccante",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 4
+        "restaurant_id" => 4,
+        "visible"       => true,
     ],
     [
         "name"          => "Pizza Margherita",
@@ -111,7 +124,8 @@ return [
         "price"         => 6,
         "description"   => "La Pizza Margherita è una creazione classica che celebra i sapori semplici dell'Italia. Base di pizza, mozzarella, pomodori e basilico si combinano per un'esplosione di sapore",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Quiche agli Spinaci e Formaggio di Capra",
@@ -119,7 +133,8 @@ return [
         "price"         => 12,
         "description"   => "Una quiche elegante e saporita, la Quiche agli Spinaci e Formaggio di Capra unisce spinaci freschi e formaggio di capra cremoso in una crosta croccante. Perfetta per pranzi o cene speciali",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Pasta al Pomodoro Fresco",
@@ -127,7 +142,8 @@ return [
         "price"         => 10,
         "description"   => "La Pasta al Pomodoro Fresco è una celebrazione della semplicità. Pomodori maturi, aglio e basilico si combinano per creare una salsa ricca e saporita per la pasta",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 4
+        "restaurant_id" => 4,
+        "visible"       => true,
     ],
     [
         "name"          => "Crostata alle Fragole",
@@ -135,7 +151,8 @@ return [
         "price"         => 6,
         "description"   => "Un dessert fresco e dolce, la Crostata alle Fragole combina una crosta friabile con fragole mature e una glassa lucida per un'esplosione di sapori fruttati",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 3
+        "restaurant_id" => 3,
+        "visible"       => true,
     ],
     [
         "name"          => "Pasta al pesto di Zucchine e Mandorle",
@@ -143,7 +160,8 @@ return [
         "price"         => 10,
         "description"   => "Un'alternativa leggera al classico pesto, il Pesto di Zucchine e Mandorle combina zucchine grigliate, mandorle tostate e pecorino per una salsa fresca e cremosa",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 4
+        "restaurant_id" => 4,
+        "visible"       => true,
     ],
     [
         "name"          => "Penne al Salmone e Asparagi",
@@ -151,7 +169,8 @@ return [
         "price"         => 13,
         "description"   => "Un piatto elegante e saporito, le Penne al Salmone e Asparagi combinano pasta penne con salmone affumicato, asparagi croccanti e una salsa leggera al limone",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Frittata al Prosciutto e Formaggio",
@@ -159,7 +178,8 @@ return [
         "price"         => 12,
         "description"   => "Una frittata abbondante e saporita, la Frittata al Prosciutto e Formaggio unisce uova, prosciutto cotto e formaggio Cheddar per una colazione o un pranzo sostanzioso",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Gnocchi alla Sorrentina",
@@ -167,7 +187,8 @@ return [
         "price"         => 12,
         "description"   => "Gnocchi alla Sorrentina",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 1
+        "restaurant_id" => 1,
+        "visible"       => true,
     ],
     [
         "name"          => "Pollo Teriyaki con Verdure",
@@ -175,7 +196,8 @@ return [
         "price"         => 15,
         "description"   => "Una pietanza asiatica appagante, il Pollo Teriyaki con Verdure combina pezzi di pollo marinati con una salsa teriyaki dolce e salata, serviti con verdure croccanti",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 3
+        "restaurant_id" => 3,
+        "visible"       => true,
     ],
     [
         "name"          => "Melanzane Parmigiana",
@@ -183,7 +205,8 @@ return [
         "price"         => 12,
         "description"   => "Una creazione italiana deliziosa e sostanziosa, la Melanzane Parmigiana combina fette di melanzane impanate, salsa di pomodoro e formaggi fondenti",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Sformato di Spinaci",
@@ -191,7 +214,8 @@ return [
         "price"         => 10,
         "description"   => "Un piatto elegante e saporito, lo Sformato di Spinaci unisce spinaci freschi con uova, panna e formaggio Parmigiano, il tutto cotto al forno per una consistenza soffice",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 3
+        "restaurant_id" => 3,
+        "visible"       => true,
     ],
     [
         "name"          => "Wraps di Pollo Caesar",
@@ -199,7 +223,8 @@ return [
         "price"         => 10,
         "description"   => "I Wraps di Pollo Caesar portano il classico sapore dell'insalata Caesar in un involucro. Pollo grigliato, lattuga croccante e salsa Caesar si fondono in un pasto leggero ma soddisfacente",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 4
+        "restaurant_id" => 4,
+        "visible"       => true,
     ],
     [
         "name"          => "Insalata di Couscous Mediterranea",
@@ -207,7 +232,8 @@ return [
         "price"         => 8,
         "description"   => "Un'insalata leggera e nutriente, l'Insalata di Couscous Mediterranea unisce couscous, verdure croccanti e olive nere, condite con un dressing al limone e olio d'oliva",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Sushi Rolls Vegetariani",
@@ -215,7 +241,8 @@ return [
         "price"         => 12,
         "description"   => "I Sushi Rolls Vegetariani sono un'opzione fresca e colorata. Avocado, carota, cetriolo e peperone si arrotolano in nori e riso, per creare un pasto leggero e gustoso",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
     [
         "name"          => "Mousse al Cioccolato",
@@ -223,7 +250,8 @@ return [
         "price"         => 6,
         "description"   => "Un dessert indulgente e vellutato, la Mousse al Cioccolato unisce cioccolato fondente fuso con panna montata, creando una mousse leggera e irresistibile",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 3
+        "restaurant_id" => 3,
+        "visible"       => true,
     ],
     [
         "name"          => "Crepes Ripiene di Ricotta e Spinaci",
@@ -231,6 +259,7 @@ return [
         "price"         => 7,
         "description"   => "Un piatto versatile, le Crepes Ripiene di Ricotta e Spinaci combinano crepes sottili con una miscela di ricotta e spinaci, il tutto condito con salsa bianca e formaggio",
         "url_image"         => "uploads/ricetta1.jpg",
-        "restaurant_id" => 2
+        "restaurant_id" => 2,
+        "visible"       => true,
     ],
 ];
