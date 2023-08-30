b0b66811dedcf30177a51fd35e2e7aa99428b9d8
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-30 11:05:12 +0200

    ciao

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -50,16 +50,18 @@
             <div class="flex flex-wrap gap-10">
                 @foreach ($products as $product)
                     <div class="w-2/5 mx-auto bg-white border border-gray-200 rounded-lg shadow">
-                        <img class="rounded-t-lg" src="{{ asset('storage/' . $product->url_image) }}" alt="{{ $product->name }}" />
-                        
-                        <div class="p-5">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                                {{ $product->name }}
-                            </h5>
-                            
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $product->description }}</p>
 
-                            <div class="flex justify-between items-center">
+                        <div class="p-5 flex justify-between flex-col">
+                            <div>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                    {{ $product->name }}
+                                </h5>
+                                
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $product->description }}</p>
+                            </div>
+                            
+
+                            <div class="flex justify-between items-center mt-auto">
                                 <form action="{{ route('admin.products.toggleProductVisibility', $product->id) }}" method="post">
                                     @csrf
                                     <input type="checkbox" name="visible" value="1" {{ $product->visible ? 'checked' : '' }} onChange="this.form.submit()">
