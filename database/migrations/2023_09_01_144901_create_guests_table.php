83c90617b5200f6ce3699a00a1cbc13fa6e756c5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-09-01 16:58:36 +0200

    add migration for mail

--- database/migrations/2023_09_01_144901_create_guests_table.php
+++ database/migrations/2023_09_01_144901_create_guests_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('guests', function (Blueprint $table) {
             $table->id();
+            $table->string('email', 50);
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('phone', 50);
+            $table->string('message', 200);
             $table->timestamps();
         });
     }
