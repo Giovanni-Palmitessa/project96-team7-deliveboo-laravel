a33ef6884bdd810ecfbbec40c9318f27238c6053
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-09-01 16:11:18 +0200

    add constructor

--- app/Mail/MailToGuest.php
+++ app/Mail/MailToGuest.php
@@ -13,14 +13,16 @@ class MailToGuest extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $lead;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($_lead)
     {
-        //
+        $this->lead = $_lead;
     }
 
     /**
