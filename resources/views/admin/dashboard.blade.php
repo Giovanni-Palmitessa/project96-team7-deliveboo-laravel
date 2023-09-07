a5118b42f0255ffeecbea762f0a142cb91f393bd
LorisMarzocchi <ml77182a@gmail.com>
2023-09-07 15:47:47 +0200

    remove product dash

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,46 +18,6 @@
             </div>
 
 
-
-            <!-- Prodotti -->
-            <div class="bg-white rounded-lg shadow-lg p-6 w-3/4 mb-6">
-                <h2 class="text-4xl font-bold text-gray-800 mb-9 text-center">I tuoi prodotti</h2>
-
-                <div class="flex flex-wrap gap-10">
-                    @foreach ($products as $product)
-                        <div class="w-2/5 mx-auto bg-white border border-gray-200 rounded-lg shadow">
-
-                            <div class="p-5 flex justify-between flex-col">
-                                <div>
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                                        {{ $product->name }}
-                                    </h5>
-
-                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $product->description }}
-                                    </p>
-                                </div>
-
-
-                                <div class="flex justify-between items-center">
-                                    <form action="{{ route('admin.products.toggleProductVisibility', $product->id) }}"
-                                        method="post">
-                                        @csrf
-                                        <input type="checkbox" name="visible" value="1"
-                                            {{ $product->visible ? 'checked' : '' }} onChange="this.form.submit()">
-                                        <label for="visible">Prodotto disponibile</label>
-                                    </form>
-
-                                    <a href="{{ route('admin.products.show', ['product' => $product]) }}"
-                                        class="focus:outline-none text-white bg-secondary hover:bg-b_hover font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Mostra
-                                        dettagli</a>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-
-            </div>
-
             <!-- Ordini -->
             <div class="bg-white rounded-lg shadow-lg p-4 md:p-6 w-full md:w-3/4 mb-6">
                 <h2 class="text-2xl font-bold text-gray-800 mb-4 text-center">Ordini</h2>
