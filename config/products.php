dd8340b174d5f11319a3ccff30190603f6792b47
DavideFarci <yank92.df@gmail.com>
2023-09-09 17:11:05 +0200

    fix prices in products.php

--- config/products.php
+++ config/products.php
@@ -4,7 +4,7 @@ return [
     [
         "name"          => "Pollo al Curry Cremoso",
         "ingredients"   => "Pollo, cipolla, aglio, zenzero, curry in polvere, latte di cocco, panna, pomodori pelati, olio d'oliva, sale, pepe, coriandolo fresco",
-        "price"         => 13,
+        "price"         => 13.00,
         "description"   => "Un piatto ricco e avvolgente, il Pollo al Curry Cremoso unisce pezzi di pollo teneri e succulenti a una salsa di curry vellutata. Servito su un letto di riso aromatico, è un comfort food con un tocco esotico",
         "url_image"         => "uploads/products/pollo-al-curry.jpg",
         "restaurant_id" => 1,
@@ -13,7 +13,7 @@ return [
     [
         "name"          => "Pollo Tikka Masala",
         "ingredients"   => "Pollo, yogurt, spezie tikka masala, pomodori, cipolla, aglio, zenzero, panna, coriandolo",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "Un'iconica pietanza indiana, il Pollo Tikka Masala offre pezzi di pollo teneri marinati e cotti in una salsa cremosa e speziata. Servito con riso basmati e coriandolo fresco.",
         "url_image"     => "uploads/products/tikka.jpg",
         "restaurant_id" => 1,
@@ -22,7 +22,7 @@ return [
     [
         "name"          => "Pollo Teriyaki con Verdure",
         "ingredients"   => "Pollo, salsa teriyaki, carote, zucchine, peperoni, cipolla, aglio, zenzero, olio di sesamo, riso, cipollotti",
-        "price"         => 15,
+        "price"         => 15.00,
         "description"   => "Una pietanza asiatica appagante, il Pollo Teriyaki con Verdure combina pezzi di pollo marinati con una salsa teriyaki dolce e salata, serviti con verdure croccanti",
         "url_image"         => "uploads/products/Pollo-teriyaki.jpg",
         "restaurant_id" => 1,
@@ -31,7 +31,7 @@ return [
     [
         "name"          => "Sushi Rolls Vegetariani",
         "ingredients"   => "Fogli di nori, riso per sushi, avocado, carota, cetriolo, peperone, aceto di riso, zucchero, sale",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "I Sushi Rolls Vegetariani sono un'opzione fresca e colorata. Avocado, carota, cetriolo e peperone si arrotolano in nori e riso, per creare un pasto leggero e gustoso",
         "url_image"         => "uploads/products/sushi.jpg",
         "restaurant_id" => 1,
@@ -41,7 +41,7 @@ return [
     [
         "name"          => "Pizza Margherita",
         "ingredients"   => "Pasta per pizza, mozzarella, pomodori maturi, basilico fresco, olio d'oliva, sale",
-        "price"         => 6,
+        "price"         => 6.00,
         "description"   => "La Pizza Margherita è una creazione classica che celebra i sapori semplici dell'Italia. Base di pizza, mozzarella, pomodori e basilico si combinano per un'esplosione di sapore",
         "url_image"         => "uploads/products/pizza.jpg",
         "restaurant_id" => 2,
@@ -50,7 +50,7 @@ return [
     [
         "name"          => "Insalata Caprese",
         "ingredients"   => "Mozzarella di bufala, pomodori maturi, basilico fresco, olio d'oliva extravergine, aceto balsamico, sale, pepe",
-        "price"         => 8,
+        "price"         => 8.00,
         "description"   => "L'Insalata Caprese è un classico italiano che celebra i sapori semplici e freschi. Mozzarella di bufala, pomodori e basilico si fondono con olio d'oliva extravergine per una combinazione irresistibile",
         "url_image"         => "uploads/products/caprese.jpg",
         "restaurant_id" => 2,
@@ -59,7 +59,7 @@ return [
     [
         "name"          => "Zuppa di Lenticchie Rosse",
         "ingredients"   => "Lenticchie rosse, carote, sedano, cipolla, aglio, pomodori a cubetti, brodo vegetale, cumino, curcuma, paprika, olio d'oliva, sale, pepe",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "Nutriente e confortante, la Zuppa di Lenticchie Rosse è ricca di proteine e fibre. Le lenticchie si uniscono a verdure e spezie per creare una zuppa dal sapore avvolgente",
         "url_image"         => "uploads/products/lenitcchie.jpg",
         "restaurant_id" => 2,
@@ -68,7 +68,7 @@ return [
     [
         "name"          => "Omelette Primaverile",
         "ingredients"   => "Uova, asparagi, piselli, formaggio feta, cipolla verde, prezzemolo, burro, sale, pepe",
-        "price"         => 7,
+        "price"         => 7.00,
         "description"   => "L'Omelette Primaverile è una colazione sana e colorata. Uova leggere si combinano con asparagi croccanti, piselli dolci e formaggio feta per iniziare la giornata con energia",
         "url_image"         => "uploads/products/omelette-primavera.jpg",
         "restaurant_id" => 2,
@@ -78,7 +78,7 @@ return [
     [
         "name"          => "Pasta al pesto di Zucchine e Mandorle",
         "ingredients"   => "Pasta, zucchine, mandorle, pecorino grattugiato, aglio, olio d'oliva, succo di limone, sale, pepe",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "Un'alternativa leggera al classico pesto, il Pesto di Zucchine e Mandorle combina zucchine grigliate, mandorle tostate e pecorino per una salsa fresca e cremosa",
         "url_image"         => "uploads/products/mandorle.jpg",
         "restaurant_id" => 3,
@@ -87,7 +87,7 @@ return [
     [
         "name"          => "Insalata di Riso Mediterranea",
         "ingredients"   => "Riso integrale, pomodori, cetrioli, olive nere, feta, cipolla rossa, prezzemolo, olio d'oliva, succo di limone, sale, pepe",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "Un'insalata completa e saporita, l'Insalata di Riso Mediterranea unisce riso integrale con pomodori, cetrioli, olive e feta, il tutto condito con un dressing all'olio d'oliva",
         "url_image"         => "uploads/products/insalata-di-riso.jpg",
         "restaurant_id" => 3,
@@ -96,7 +96,7 @@ return [
     [
         "name"          => "Pasta al Pomodoro Fresco",
         "ingredients"   => "Pasta, pomodori maturi, aglio, basilico fresco, olio d'oliva, sale, pepe",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "La Pasta al Pomodoro Fresco è una celebrazione della semplicità. Pomodori maturi, aglio e basilico si combinano per creare una salsa ricca e saporita per la pasta",
         "url_image"         => "uploads/products/spaghetti.jpg",
         "restaurant_id" => 3,
@@ -105,7 +105,7 @@ return [
     [
         "name"          => "Risotto ai Funghi Porcini",
         "ingredients"   => "Riso Arborio, funghi porcini secchi, brodo vegetale, cipolla, vino bianco, burro, parmigiano grattugiato, prezzemolo, sale, pepe",
-        "price"         => 18,
+        "price"         => 18.00,
         "description"   => "Un comfort food di lusso, il Risotto ai Funghi Porcini unisce il ricco sapore dei funghi porcini con la cremosità del risotto. Un piatto caldo e avvolgente per le serate fresche",
         "url_image"         => "uploads/products/risotto-ai-funghi-porcini.jpg",
         "restaurant_id" => 3,
@@ -115,7 +115,7 @@ return [
     [
         "name"          => "Melanzane Parmigiana",
         "ingredients"   => "Melanzane, salsa di pomodoro, mozzarella, parmigiano grattugiato, basilico fresco, uova, pangrattato, olio d'oliva, sale, pepe",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "Una creazione italiana deliziosa e sostanziosa, la Melanzane Parmigiana combina fette di melanzane impanate, salsa di pomodoro e formaggi fondenti",
         "url_image"         => "uploads/products/parmigiana.jpg",
         "restaurant_id" => 4,
@@ -124,7 +124,7 @@ return [
     [
         "name"          => "Quiche agli Spinaci e Formaggio di Capra",
         "ingredients"   => "Pasta per crosta di quiche, spinaci freschi, formaggio di capra, uova, panna, cipolla, noce moscata, sale, pepe",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "Una quiche elegante e saporita, la Quiche agli Spinaci e Formaggio di Capra unisce spinaci freschi e formaggio di capra cremoso in una crosta croccante. Perfetta per pranzi o cene speciali",
         "url_image"         => "uploads/products/Torta-salata.jpg",
         "restaurant_id" => 4,
@@ -133,7 +133,7 @@ return [
     [
         "name"          => "Insalata di Couscous Mediterranea",
         "ingredients"   => "Couscous, pomodori, cetrioli, peperoni, olive nere, cipolla rossa, prezzemolo, limone, olio d'oliva, sale, pepe",
-        "price"         => 8,
+        "price"         => 8.00,
         "description"   => "Un'insalata leggera e nutriente, l'Insalata di Couscous Mediterranea unisce couscous, verdure croccanti e olive nere, condite con un dressing al limone e olio d'oliva",
         "url_image"         => "uploads/products/cous.jpg",
         "restaurant_id" => 4,
@@ -142,7 +142,7 @@ return [
     [
         "name"          => "Insalata di Quinoa Mediterranea",
         "ingredients"   => "Quinoa, pomodori, cetrioli, olive, feta, cipolla rossa, prezzemolo, succo di limone, olio d'oliva, sale, pepe",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "Un'insalata fresca e nutriente che fonde la quinoa con sapori mediterranei. Pomodori dolci, cetrioli croccanti, olive salate e feta cremosa si uniscono per creare un'esplosione di gusto",
         "url_image"         => "uploads/products/quinoa.jpg",
         "restaurant_id" => 4,
@@ -152,7 +152,7 @@ return [
     [
         "name"          => "Crostata alle Fragole",
         "ingredients"   => "Pasta frolla, fragole, zucchero, gelatina di fragole, succo di limone",
-        "price"         => 6,
+        "price"         => 6.00,
         "description"   => "Un dessert fresco e dolce, la Crostata alle Fragole combina una crosta friabile con fragole mature e una glassa lucida per un'esplosione di sapori fruttati",
         "url_image"         => "uploads/products/fragole.jpg",
         "restaurant_id" => 5,
@@ -161,7 +161,7 @@ return [
     [
         "name"          => "Crepes Ripiene di Ricotta e Spinaci",
         "ingredients"   => "Crepes, ricotta, spinaci, uova, latte, farina, noce moscata, burro, formaggio grattugiato",
-        "price"         => 7,
+        "price"         => 7.00,
         "description"   => "Un piatto versatile, le Crepes Ripiene di Ricotta e Spinaci combinano crepes sottili con una miscela di ricotta e spinaci, il tutto condito con salsa bianca e formaggio",
         "url_image"         => "uploads/products/crepes-ric.jpg",
         "restaurant_id" => 5,
@@ -170,7 +170,7 @@ return [
     [
         "name"          => "Penne al Salmone e Asparagi",
         "ingredients"   => "Penne, salmone affumicato, asparagi, panna, succo di limone, cipolla, vino bianco, olio d'oliva, sale, pepe",
-        "price"         => 13,
+        "price"         => 13.00,
         "description"   => "Un piatto elegante e saporito, le Penne al Salmone e Asparagi combinano pasta penne con salmone affumicato, asparagi croccanti e una salsa leggera al limone",
         "url_image"         => "uploads/products/penna-salmone.jpg",
         "restaurant_id" => 5,
@@ -179,7 +179,7 @@ return [
     [
         "name"          => "Frittata al Prosciutto e Formaggio",
         "ingredients"   => "Uova, prosciutto cotto, formaggio Cheddar, cipolla, prezzemolo, burro, sale, pepe",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "Una frittata abbondante e saporita, la Frittata al Prosciutto e Formaggio unisce uova, prosciutto cotto e formaggio Cheddar per una colazione o un pranzo sostanzioso",
         "url_image"         => "uploads/products/rotolo-di-frittata.jpg",
         "restaurant_id" => 5,
@@ -189,7 +189,7 @@ return [
     [
         "name"          => "Gnocchi alla Sorrentina",
         "ingredients"   => "Gnocchi, pomodori a cubetti, mozzarella, basilico fresco, olio d'oliva, aglio, sale, pepe",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "Gnocchi alla Sorrentina",
         "url_image"         => "uploads/products/gnocchi.jpg",
         "restaurant_id" => 6,
@@ -198,7 +198,7 @@ return [
     [
         "name"          => "Tagliatelle al Salmone Affumicato",
         "ingredients"   => "Tagliatelle, salmone affumicato, panna, succo di limone, cipolla, vino bianco, olio d'oliva, sale, pepe, aneto fresco",
-        "price"         => 16,
+        "price"         => 16.00,
         "description"   => "Un piatto elegante e delizioso, le Tagliatelle al Salmone Affumicato sono una fusione di pasta fresca e salmone delicato. Una salsa leggera al limone completa questa prelibatezza",
         "url_image"         => "uploads/products/tagl-salm.jpg",
         "restaurant_id" => 6,
@@ -207,7 +207,7 @@ return [
     [
         "name"          => "Salmone alla Griglia con Salsa di Mango",
         "ingredients"   => "Salmone, mango, peperoncino, cipolla rossa, coriandolo, lime, olio d'oliva, sale, pepe",
-        "price"         => 16,
+        "price"         => 16.00,
         "description"   => "Il Salmone alla Griglia con Salsa di Mango è una combinazione di sapori freschi e contrastanti. Il salmone grigliato è arricchito da una salsa di mango dolce e piccante",
         "url_image"         => "uploads/products/salm-sesamo.jpg",
         "restaurant_id" => 6,
@@ -216,7 +216,7 @@ return [
     [
         "name"          => "Gamberi Aglio e Limone",
         "ingredients"   => "Gamberi, aglio, limone, prezzemolo, olio d'oliva, sale, pepe",
-        "price"         => 18,
+        "price"         => 18.00,
         "description"   => "I Gamberi Aglio e Limone sono una deliziosa prelibatezza marina. Gamberi succosi vengono cotti con aglio fragrante e succo di limone per un piatto fresco e leggero",
         "url_image"         => "uploads/products/gamberi-al-limone.jpg",
         "restaurant_id" => 6,
@@ -226,7 +226,7 @@ return [
     [
         "name"          => "Pollo al Rosmarino con Patate",
         "ingredients"   => "Pollo, patate, rosmarino, aglio, limone, olio d'oliva, sale, pepe",
-        "price"         => 15,
+        "price"         => 15.00,
         "description"   => "Un piatto rustico e aromatico, il Pollo al Rosmarino con Patate unisce pezzi di pollo succulenti con patate arrosto profumate al rosmarino. Un comfort food dal sapore intenso",
         "url_image"         => "uploads/products/pollo-forno.jpg",
         "restaurant_id" => 7,
@@ -235,7 +235,7 @@ return [
     [
         "name"          => "Spaghetti alla Carbonara",
         "ingredients"   => "Spaghetti, pancetta, uova, formaggio pecorino, pepe nero, olio d'oliva, sale",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "Un classico italiano, gli Spaghetti alla Carbonara combinano uova, pancetta croccante e formaggio pecorino. Una pasta cremosa e saporita che conquista ogni palato",
         "url_image"         => "uploads/products/Spaghetti-alla-carbonara.jpg",
         "restaurant_id" => 7,
@@ -244,7 +244,7 @@ return [
     [
         "name"          => "Lasagna al Forno con Besciamella",
         "ingredients"   => "Pasta per lasagna, carne macinata, pomodoro, cipolla, aglio, mozzarella, besciamella, parmigiano reggiano, basilico",
-        "price"         => 12,
+        "price"         => 12.00,
         "description"   => "Una classica comfort food, la Lasagna al Forno con Besciamella combina strati di pasta, carne saporita, mozzarella filante e una deliziosa besciamella. Un'esplosione di sapori e consistenze.",
         "url_image"     => "uploads/products/lasagna.jpg",
         "restaurant_id" => 7,
@@ -253,7 +253,7 @@ return [
     [
         "name"          => "Sformato di Spinaci",
         "ingredients"   => "Spinaci freschi, uova, panna, formaggio Parmigiano, burro, noce moscata, sale, pepe",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "Un piatto elegante e saporito, lo Sformato di Spinaci unisce spinaci freschi con uova, panna e formaggio Parmigiano, il tutto cotto al forno per una consistenza soffice",
         "url_image"         => "uploads/products/tortino.jpg",
         "restaurant_id" => 7,
@@ -263,7 +263,7 @@ return [
     [
         "name"          => "Wraps di Pollo Caesar",
         "ingredients"   => "Tortillas, pollo grigliato, lattuga romana, parmigiano grattugiato, salsa Caesar, crouton, pepe nero",
-        "price"         => 10,
+        "price"         => 10.00,
         "description"   => "I Wraps di Pollo Caesar portano il classico sapore dell'insalata Caesar in un involucro. Pollo grigliato, lattuga croccante e salsa Caesar si fondono in un pasto leggero ma soddisfacente",
         "url_image"         => "uploads/products/wrap-pollo.jpg",
         "restaurant_id" => 8,
@@ -272,7 +272,7 @@ return [
     [
         "name"          => "Tacos al Pastor",
         "ingredients"   => "Carne di maiale marinata, ananas, cipolla, coriandolo, lime, tortillas di mais",
-        "price"         => 9,
+        "price"         => 9.00,
         "description"   => "I Tacos al Pastor sono una prelibatezza messicana. Carne di maiale marinata viene cucinata allo spiedo, servita in tortillas di mais e guarnita con ananas, cipolla, coriandolo e una spruzzata di lime.",
         "url_image"     => "uploads/products/wrap.jpg",
         "restaurant_id" => 8,
@@ -281,7 +281,7 @@ return [
     [
         "name"          => "Tacos di Pollo alla Griglia",
         "ingredients"   => "Pollo, tortillas di mais, lime, coriandolo, cipolla rossa, peperoncino jalapeño, salsa di pomodoro, avocado, formaggio Cheddar, panna acida",
-        "price"         => 8,
+        "price"         => 8.00,
         "description"   => "I Tacos di Pollo alla Griglia portano il sapore dell'estate nel tuo piatto. Il pollo marinato e grigliato è avvolto in tortillas calde e accompagnato da una selezione di salse e condimenti",
         "url_image"         => "uploads/products/Tacos-di-pollo.jpg",
         "restaurant_id" => 8,
@@ -290,7 +290,7 @@ return [
     [
         "name"          => "Mousse al Cioccolato",
         "ingredients"   => "Cioccolato fondente, panna, uova, zucchero, vaniglia",
-        "price"         => 6,
+        "price"         => 6.00,
         "description"   => "Un dessert indulgente e vellutato, la Mousse al Cioccolato unisce cioccolato fondente fuso con panna montata, creando una mousse leggera e irresistibile",
         "url_image"         => "uploads/products/mousse.jpg",
         "restaurant_id" => 8,
