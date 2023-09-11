512dc7993a7cfb569642dda2833ad3cf1c211a2d
DavideFarci <yank92.df@gmail.com>
2023-09-11 12:51:29 +0200

    fix style form edit product

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.base')
 @section('contents')
-<h1 class="text-center text-3xl mb-2 text-secondary">MODIFICA IL TUO PRODOTTO</h1>
+<h1 class="text-center font-bold text-3xl mt-28 mb-2 text-secondary">MODIFICA IL TUO PRODOTTO</h1>
 <div class="container mx-auto max-w-screen-xl px-2 mb-2">
     <form
     method="POST"
@@ -11,35 +11,35 @@
         @csrf
         @method('put')
         <div class="mb-3">
-            <label for="name" class="form-label">Nome</label>
+            <label for="name" class="form-label block mb-2 text-sm font-medium text-secondary">Nome</label>
             <input type="text" 
-            class="form-control block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" id="product_name_edit" 
+            class="form-control bg-gray-50 border border-primary text-gray-900 text-sm rounded-lg focus:ring-secondary focus:border-secondary block w-full p-2.5" id="product_name_edit" 
             name="name" 
             value="{{old('name', $product->name)}}">
         </div>
         <div class="mb-2 text-sm error" id="ProductNameError"></div>
 
         <div class="mb-3">
-            <label for="ingredients" class="form-label">Ingredienti</label>
+            <label for="ingredients" class="form-label block mb-2 text-sm font-medium text-secondary">Ingredienti</label>
             <input type="text" 
-            class="form-control block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" id="product_ingredients_edit" 
+            class="form-control bg-gray-50 border border-primary text-gray-900 text-sm rounded-lg focus:ring-secondary focus:border-secondary block w-full p-2.5" id="product_ingredients_edit" 
             name="ingredients" 
             value="{{old('ingredients', $product->ingredients)}}">
         </div>
         <div class="mb-2 text-sm error" id="ProductIngredientsError"></div>
 
         <div class="mb-3">
-            <label for="price" class="form-label">Prezzo</label>
+            <label for="price" class="form-label block mb-2 text-sm font-medium text-secondary">Prezzo</label>
             <input type="text" 
-            class="form-control block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" id="product_price_edit" 
+            class="form-control bg-gray-50 border border-primary text-gray-900 text-sm rounded-lg focus:ring-secondary focus:border-secondary block w-full p-2.5" id="product_price_edit" 
             name="price" 
             value="{{old('price', $product->price)}}">
         </div>
         <div class="mb-2 text-sm error" id="ProductPriceError"></div>
 
         <div class="mb-3">
-            <label for="description" class="form-label">Descrizione</label>
-            <textarea class="form-control block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" 
+            <label for="description" class="form-label block mb-2 text-sm font-medium text-secondary">Descrizione</label>
+            <textarea class="form-control bg-gray-50 border border-primary text-gray-900 text-sm rounded-lg focus:ring-secondary focus:border-secondary block w-full p-2.5" 
             name="description" 
             id="product_description_edit"
             rows="3">{{ old('description', $product->description) }}</textarea>
@@ -54,7 +54,7 @@
         <div class="mb-2 text-sm error" id="url_imageError"></div>
 
 
-    <button type="submit" class="rounded-lg bg-blue-500 hover:bg-blue-700 font-medium text-sm px-5 py-2.5 text-center text-white">Salva</button>
+    <button type="submit" class="text-white bg-secondary hover:bg-b_hover focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center shadow-md">Salva</button>
 </form>
 
         
